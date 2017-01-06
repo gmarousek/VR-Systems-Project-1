3826707e2ebfa95ea9e44ccf954a14b17a72c490
gmarousek <graham.marousek@gmail.com>
2017-01-05 22:34:13 -0700

    An old VR project
    The camera follows a set path around a maze, then stops. A very simple
    virtual reality project, as the camera can be split for viewing in a
    headset.

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	public GameObject[] cameraPoints;
+	public GameObject camera;
+	private bool next;
+	private int index;
+	private GameObject point;
+	public float speed;
+	public float look;
+
+	// Use this for initialization
+	void Start () {
+		next = true;
+		index = 0;
+		point = cameraPoints [0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (index == 12 && camera.transform.position == point.transform.position) {
+			index++;
+			point = cameraPoints [index];
+
+			camera.transform.position = point.transform.position;
+			camera.transform.rotation = Quaternion.Lerp (camera.transform.rotation, point.transform.rotation, look);
+
+		} else {
+			if (next && index <= 19) {
+				point = cameraPoints [index];
+				next = false;
+
+			} else if (camera.transform.position == point.transform.position) {
+				next = true;
+				index++;
+			}
+			if (index <= 20) {
+				camera.transform.position = Vector3.MoveTowards (camera.transform.position, point.transform.position, speed);
+				camera.transform.rotation = Quaternion.Lerp (camera.transform.rotation, point.transform.rotation, look);
+			}
+		}
+	}
+}
---
+++ Assets/Stereoscopy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+//using System.Collections;
+
+public class Stereoscopy : MonoBehaviour {
+
+	Camera camera1;
+//	Camera camera2;
+	public float ipd;
+//	Vector3 pos1;
+//	Vector3 pos2;
+
+
+	// Use this for initialization
+	void Start () {
+		if (camera1 == null) {
+			camera1 = Instantiate (Camera.main);
+			camera1.GetComponent<Stereoscopy> ().enabled = false;
+			camera1.GetComponent<CameraMove> ().enabled = false;
+
+			Camera.main.transform.position = new Vector3 (
+				Camera.main.transform.position.x - (ipd / 2),
+				Camera.main.transform.position.y,
+				Camera.main.transform.position.z);
+
+			camera1.transform.parent = Camera.main.transform;
+			camera1.transform.position = new Vector3 (
+				Camera.main.transform.position.x + (ipd / 2),
+				Camera.main.transform.position.y,
+				Camera.main.transform.position.z);
+		}
+
+		Camera.main.rect = new Rect (0, 0, .5f, 1);
+		camera1.rect = new Rect (.5f, 0, 1, 1);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
